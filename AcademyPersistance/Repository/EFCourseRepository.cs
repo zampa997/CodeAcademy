afd8fee1ed2ea255fdaff94537515186161ede50
bloodkill23 <78487863+bloodkill23@users.noreply.github.com>
2021-10-18 12:08:26 +0200

    xfgbdzg

--- AcademyPersistance/Repository/EFCourseRepository.cs
+++ AcademyPersistance/Repository/EFCourseRepository.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using AcademyEFPersistance.EFContext;
 
 namespace AcademyEFPersistance.Repository
