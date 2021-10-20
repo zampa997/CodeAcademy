d8924fe19f49bb4bb2247e370c130df1dcb8638d
samuel rizzi <samuelrizzi.sr@gmail.com>
2021-10-20 09:58:45 +0200

    sistemato instructorDTO

--- CodeAcademyWeb/DTOs/InstructorDTO.cs
+++ CodeAcademyWeb/DTOs/InstructorDTO.cs
@@ -9,7 +9,7 @@ namespace CodeAcademyWeb.DTOs
 	{
 		public long Id { get; set; }
 		public string Firstname { get; set; }
-		public string Surname { get; set; }
+		public string Lastname { get; set; }
 		public string DateOfBirth { get; set; }
 		public string Address { get; set; }
 		public string City { get; set; }
